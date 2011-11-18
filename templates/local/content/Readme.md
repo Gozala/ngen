0b9ece8d0a4df1bb416a985ec22c71b2202347b8
Irakli Gozalishvili <rfobic@gmail.com>
2011-11-18 10:50:13 -0800

    Adding local template.

---
+++ templates/local/content/Readme.md
@@ -0,0 +1,11 @@
+# {{project}} #
+
+{{description}}
+
+## Install ##
+
+    npm install {{project}}
+
+## Require ##
+
+    var {{project}} = require('https!raw.github.com/Gozala/{{project}}/v0.0.1/{{project}}.js')
---
+++ templates/local/index.js
@@ -0,0 +1,31 @@
+var spawn = require('child_process').spawn;
+
+exports.variables = {
+    project: 'Project name: ',
+    description: 'Project description: ',
+    date: function(values, callback) {
+        var date = new Date;
+        var year = date.getFullYear();
+        var month = String(date.getMonth() + 1);
+        month = month.length === 2 ? month : 0 + month;
+        var day = String(date.getDate());
+        day = day.length === 2 ? day : 0 + day;
+        callback([year, month, day].join('-'));
+    },
+    name: function(values, callback) {
+        spawn('git', [
+            '--bare',
+            'config',
+            '--global',
+            'user.name'
+        ]).stdout.once('data', callback);
+    },
+    email: function(values, callback) {
+        spawn('git', [
+            '--bare',
+            'config',
+            '--global',
+            'user.email'
+        ]).stdout.once('data', callback);
+    }
+};
